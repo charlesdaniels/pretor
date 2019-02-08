1d8951e7d4ec15e84e4f9210ca8603c394baba70
Charles Daniels <cdaniels@fastmail.com>
2019-02-08 14:33:27 -0500

    more detailed linter output

--- scripts/run_linter.sh
+++ scripts/run_linter.sh
@@ -11,6 +11,8 @@ cd "$(dirname "$0")/.."
 
 if [ "$(flake8 pretor | wc -l)" -gt "$RATCHET" ] ; then
 	echo "too many linter errors, exceeded $RATCHET"
+	echo "$(flake8 pretor | wc -l) errors"
+	flake8 pretor
 	exit 1
 else
 	exit 0
