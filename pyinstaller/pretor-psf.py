659cecc511aabdbe1453d494dcaa7819f5d24ec9
Charles Daniels <cdaniels@fastmail.com>
2019-02-09 23:18:22 -0500

    run smoke tests with pyinstaller

---
+++ pyinstaller/pretor-psf.py
@@ -0,0 +1,4 @@
+from pretor import psf
+
+if __name__ == "__main__":
+    psf.psf_cli()
--- scripts/run_smoketests.sh
+++ scripts/run_smoketests.sh
@@ -4,6 +4,8 @@ FAILED=0
 
 cd "$(dirname "$0")"
 
+echo "pretor-psf is: $(which pretor-psf)" 1>&2
+
 for f in ./smoketests/*.sh ; do
 	if ! sh "$f" ; then
 		FAILED="$(expr "$FAILED" + 1)"
